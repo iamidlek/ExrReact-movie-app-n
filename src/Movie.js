525a0cc2b0ea5f03845e82cfe5fe8fc1b207d2c8
iamidlek <iamidlek@naver.com>
2021-10-23 18:17:12 +0900

    get movie data

---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// state가 필요없으므로 class를 사용하지 않음
+function Movie({ id, year, title, summary, poster }) {
+  return <h4>{title}</h4>;
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
+
+export default Movie;
