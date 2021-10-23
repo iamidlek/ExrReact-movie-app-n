ab7ea62c54fb9301ffb50db437bc9544ca1814a1
iamidlek <iamidlek@naver.com>
2021-10-23 21:47:13 +0900

    route and props 설정 완료

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,36 +4,36 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 // state가 필요없으므로 class를 사용하지 않음
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <Link
-      to={{
-        pathname: "/movie-detail",
-        state: {
-          year,
-          title,
-          summary,
-          poster,
-          genres
-        }
-      }}
-    >
-      <div className="movie">
-        <img src={poster} alt={title} title={title} />
-        <div className="movie__data">
-          <h3 className="movie__title">{title}</h3>
-          <h5 className="movie__year">{year}</h5>
-          <ul className="movie__genres">
-            {genres.map((genre, index) => (
-              <li key={index} className="genres__genre">
-                {genre}
-              </li>
-            ))}
-          </ul>
-          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres
+          }
+        }}
+      >
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
-      </div>
     </Link>
+  </div>
   );
 }
 
