d752338360823f08b3e063c07becf34b2cdd5943
iamidlek <iamidlek@naver.com>
2021-10-23 18:31:07 +0900

    구조 잡기 및 스타일 적용 방법 1

--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 // state가 필요없으므로 class를 사용하지 않음
-function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
-}
-
-Movie.propTypes = {
-  id: PropTypes.number.isRequired,
-  year: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
