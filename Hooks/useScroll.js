aeeb9ba84e299d82c48921d9b8b371fc92cdf163
iamidlek <iamidlek@naver.com>
2021-10-28 12:28:58 +0900

    hook useScroll

---
+++ Hooks/useScroll.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const useScroll = () => {
+  const [state, setState] = useState({ x: 0, y: 0 });
+
+  const onScroll = () => {
+    setState({ y: window.scrollY, x: window.scrollX });
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return state;
+};
+
+function App() {
+  const { y } = useScroll();
+
+  return (
+    <div className="App" style={{ height: "300vh" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
+        Hello
+      </h1>
+    </div>
+  );
+}
+
+export default App;
